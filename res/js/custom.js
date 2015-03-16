70d230632812f68d6316508a68c304cede98f578
Lukas Schreiner <lukas.schreiner@gmail.com>
2015-03-16 19:58:07 +0100

    Wrong offset used

--- res/js/custom.js
+++ res/js/custom.js
@@ -32,7 +32,7 @@ $(function () {
     // Countdown
     $('.countdown').downCount({
         date: '09/12/2015 12:00:00',
-        offset: +10
+        offset: null
     });
 
     $('#wishorderform').ajaxForm(function(returnVal) {
